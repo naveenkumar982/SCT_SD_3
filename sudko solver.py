e3c63fa390449f5e5083610925d939c64d0b8863
naveenkumar982 <naveenkumar982@outlook.com>
2026-01-05 11:20:00 +0530

    Add files via upload

---
+++ sudko solver.py
@@ -0,0 +1,41 @@
+def solve(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                for num in range(1, 10):
+                    if valid(board, i, j, num):
+                        board[i][j] = num
+                        if solve(board):
+                            return True
+                        board[i][j] = 0
+                return False
+    return True
+
+def valid(board, row, col, num):
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[startRow + i][startCol + j] == num:
+                return False
+    return True
+
+board = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+
+solve(board)
+for row in board:
+    print(row)
